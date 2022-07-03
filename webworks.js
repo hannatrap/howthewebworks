127abb4a37571be5e8e1c4cf2f66500b31b22b30
hannatrap <107367091+hannatrap@users.noreply.github.com>
2022-07-03 11:06:31 -0500

    Add files via upload

---
+++ webworks.js
@@ -0,0 +1,12 @@
+//solidify terminology 
+
+//HTTP: Hyper Text Transfer Protocol for browsers and servers to communicate
+//URL: Uniform Resource Locator the address to get where you need to go on the internet
+//DNS: translates Domain Name System translates from human to machine IP addresses
+//Query string: key value pairs in the url taken from a form or search and might change the destination
+//HTTP verbs: GET are requests that do not change the servers data and POST are requests that are submissions and change the server data
+//HTTP request: sending your browser to a webpage to a specific URL
+//HTTP response: what you get when you send a request - the HTML text
+//HTTP header: additional information about the response and requests ex request: host, cookies, accept, ex response: last-modified, set-cookie
+//what happens when you type provided link into a browser? browser uses DNS to fin IP address - requests to that IP address - receive a response from the server - make a DOM from that HTML and retrieves any other needed resources - broser makes HTTP requests and receives servers responses 
+
